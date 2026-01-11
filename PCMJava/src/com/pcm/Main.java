46f09b6240bd25e8e4e238a641779223b7ef4c5f
abhyuday25 <sinhaabhyuday25@gmail.com>
2026-01-11 13:26:07 +0530

    Updated java code

--- PCMJava/src/com/pcm/Main.java
+++ PCMJava/src/com/pcm/Main.java
@@ -1,6 +1,8 @@
 package com.pcm;
 public class Main {
 public static void main(String[] args) {
-System.out.println("Java program executed using Jenkins CI");
+	System.out.println("NAME -- Abhyuday Sinha");
+	System.out.println("REGISTRATION NUMBER -- 23MIC0036");
+	System.out.println("Java program executed using Jenkins CI");
 }
 }
