d558e7a1ad032d6523898d28161bb3f724363216
Abhyuday Sinha <sinhaabhyuday25@gmail.com>
2026-02-17 02:48:03 +0530

    Update code

--- PCMJava/src/com/pcm/Main.java
+++ PCMJava/src/com/pcm/Main.java
@@ -1,9 +1,12 @@
 package com.pcm;
+import java.util.*;
 public class Main {
 public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
 	System.out.println("NAME -- Abhyuday Sinha");
 	System.out.println("REGISTRATION NUMBER -- 23MIC0036");
 	System.out.println("Java program executed using Jenkins CI");
 	System.out.println("Jenkins-GitHUB Integration Successful");
+	sc.close();
 }
 }
